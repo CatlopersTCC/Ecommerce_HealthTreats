2fd67163e07bfd7e8eeedcb7cdda2a00700ef5ba
MatheusGamaRussi <matheus.russi@etec.sp.gov.br>
2024-11-02 14:09:03 -0300

    Modificando a estilização da tela de cadastro de usuário + endereco

--- WebEcommerce/WebEcommerce/obj/Debug/net8.0/WebEcommerce.AssemblyInfo.cs
+++ WebEcommerce/WebEcommerce/obj/Debug/net8.0/WebEcommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00dba61a4cee9a9eae6b3d82c4171de08b6ef4a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ba5d790cc564a1b15049aee6a65df47a1e9f069")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
