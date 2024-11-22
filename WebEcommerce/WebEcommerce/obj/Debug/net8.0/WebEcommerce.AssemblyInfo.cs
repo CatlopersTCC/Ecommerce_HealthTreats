698b2c61b8842e58bd431bb764576976d837a870
MatheusGamaRussi <matheus.russi@etec.sp.gov.br>
2024-11-22 18:46:05 -0300

    Refinando o front-end

--- WebEcommerce/WebEcommerce/obj/Debug/net8.0/WebEcommerce.AssemblyInfo.cs
+++ WebEcommerce/WebEcommerce/obj/Debug/net8.0/WebEcommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9208a6d329b55cc91b5cb430a7fd4b5d73c4e108")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257dcb07b1bfac1d17812fc17a194987abfa8ed6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
